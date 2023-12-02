84ab6ff55f4d30194e915e464bc8834fa5147580
talineshawwaa <talineshawwa15@gmail.com>
2023-12-02 19:47:57 +0100

    models for sale selector

--- wisam/modelsSaleSelector.py
+++ wisam/modelsSaleSelector.py
@@ -1,9 +1,6 @@
 from django.db import models
-from wisam.modelShoes import Shoes
+from wisam.modelShoes import Sales
 
-class ShoeSelectors(models.Model):
-    shoe = models.OneToOneField(Shoes, on_delete=models.CASCADE)
-    brand_selector = models.CharField(max_length=255, default='css-t7k2e1')
-    model_name_selector = models.CharField(max_length=255, default='css-t7k2e1')
-    size_selector = models.CharField(max_length=255, default='css-r6z5ec')
-    price_selector = models.CharField(max_length=255, default='css-cptlwn')
+class SalesSelectors(models.Model):
+    sales = models.OneToOneField(Sales, on_delete=models.CASCADE)
+    number_sold_last_3_days_selector = models.CharField(max_length=255, default='css-6zmw74')
